9294bac222154b7a844e9bac500f2595e4764576
James Halliday <mail@substack.net>
2014-02-18 13:09:27 -0800

    example works

--- index.js
+++ index.js
@@ -3,22 +3,30 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (opts, cb) {
-    if (typeof opts === 'string') opts.dir = opts;
+    if (typeof opts === 'string') opts = { dir: opts };
     var dirs = find(opts.dir);
     var parcels = {};
+    var pending = 1;
+    
     dirs.on('file', function (file) {
         var full = path.resolve(opts.dir, file);
         if (path.basename(file) === 'package.json') {
+            pending ++;
             fs.readFile(full, 'utf8', function (err, src) {
                 try { var pkg = JSON.parse(src) }
                 catch (err) { return cb(err) }
                 if (pkg && pkg.view) {
                     parcels[full] = pkg;
                 }
+                if (--pending === 0) done();
             });
         }
     });
     dirs.on('end', function () {
-        cb(null, parcels);
+        if (--pending === 0) done();
     });
+    
+    function done () {
+        cb(null, parcels);
+    }
 };
