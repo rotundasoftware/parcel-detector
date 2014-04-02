964a0df484dc0834404a56b92553743d793fbc17
David Beck <david@rotundasoftware.com>
2014-04-02 20:18:14 -0300

    passing tests

--- test/detect.js
+++ test/detect.js
@@ -1,8 +1,19 @@
 var detect = require('../');
 var test = require('tape');
 
-var expected = {"/Users/david_beck/Documents/git/parcel-finder/test/detect/page1/package.json":{"view":"view.html","main":"main.js","__mainPath":"/Users/david_beck/Documents/git/parcel-finder/test/detect/page1/main.js"},"/Users/david_beck/Documents/git/parcel-finder/test/detect/page2/package.json":{"view":"render.jade","__mainPath":"/Users/david_beck/Documents/git/parcel-finder/test/detect/page2/index.js"}};
+var expected = {};
+
 var dir = __dirname + '/detect';
+expected[dir + '/page1/package.json'] = {
+	"view":"view.html",
+	"main":"main.js",
+	"__mainPath": dir + "/page1/main.js"
+};
+
+expected[dir + '/page2/package.json'] = {
+	"view":"render.jade",
+	"__mainPath": dir + "/page2/index.js"
+};
 
 test('detect', function (t) {
     t.plan(2);
--- test/nested.js
+++ test/nested.js
@@ -1,8 +1,20 @@
 var detect = require('../');
 var test = require('tape');
 
-var expected = {"/Users/david_beck/Documents/git/parcel-finder/test/nested/page1/package.json":{"view":"view.html","main":"main.js","__mainPath":"/Users/david_beck/Documents/git/parcel-finder/test/nested/page1/main.js"},"/Users/david_beck/Documents/git/parcel-finder/test/nested/page2/package.json":{"view":"render.jade","__mainPath":"/Users/david_beck/Documents/git/parcel-finder/test/nested/page2/index.js"}};
+var expected = {};
 var dir = __dirname + '/nested';
+expected[dir + '/page1/package.json'] = {
+	"view":"view.html",
+	"main":"main.js",
+	"__mainPath": dir + "/page1/main.js"
+};
+
+expected[dir + '/page2/package.json'] = {
+	"view":"render.jade",
+	"__mainPath": dir + "/page2/index.js"
+};
+
+require('./nested/page1/package.json');
 
 test('nested', function (t) {
     t.plan(2);
